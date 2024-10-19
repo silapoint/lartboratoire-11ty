5d58b12f7c2a4fd6da8f214d8eae08d5ca5c72a1
sila. <sila+gh@mailbox.org>
2024-10-19 17:02:53 +0200

    [update] sitemap.xml meta link + repo link

--- src/_data/site.js
+++ src/_data/site.js
@@ -8,13 +8,13 @@ module.exports = {
     email: 'contact[arobase]lartboratoire[point]fr',
     twitter: '@artboratoire',
     header: 'img/square_search_engines_lartboratoire.webp',
-    repo: 'https://gitlab.com/lartboratoire/artboratoire-wordpress-theme'
+    repo: 'https://github.com/silapoint/silapoint.github.io'
   },
   noscript: {
     "header": "L'art a parfois besoin d'un peu d'aide pour être affiché...",
     "text": [
-      "Ce site fonctionne un peu mieux avec le JavaScript activé, malgré tous mes efforts pour le rendre accessible au maximum. Il ne contient aucun tracker externe, ni de publicité, promis. Si vous le souhaitez, vous pouvez le vérifier par vous-même en consultant son <a href='https://gitlab.com/lartboratoire/artboratoire-wordpress-theme' target='_blank'>repo public</a>. \
-      Vous pouvez aussi visiter <a href='https://lartboratoire.fr/politique-confidentialite/'>cette page</a> pour davantage d'informations sur la politique de confidentialité mise en place."
+      "Ce site fonctionne un peu mieux avec le JavaScript activé, malgré tous mes efforts pour le rendre accessible au maximum. Il ne contient aucun tracker externe, ni de publicité, promis. Si vous le souhaitez, vous pouvez le vérifier par vous-même en consultant son <a href='https://github.com/silapoint/silapoint.github.io' target='_blank'>repo public</a>. \
+      Vous pouvez aussi visiter <a href='/politique-confidentialite/'>cette page</a> pour davantage d'informations sur la politique de confidentialité mise en place."
     ]
   }
 }
